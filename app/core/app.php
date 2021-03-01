ea9edc30e11842e387a01de28366c253cd062ef2
edvie <edvi.edvi@yahoo.com>
2021-03-01 23:00:59 +0200

    completed app.php

--- app/core/app.php
+++ app/core/app.php
@@ -2,13 +2,34 @@
 
 class App
 {
+    protected $controller = "home";
+    protected $method = "index";
+    protected $params = [];
+
+
+
     public function __construct()
     {
 
         $url = $this->splitURL();
-        echo "<pre>";
-        print_r($url);
-        echo "</pre>";
+        if (file_exists("../app/controllers/" . strtolower($url[0]). ".php"))
+        {
+            $this->controller = strtolower($url[0]);
+            unset($url[0]);
+        }
+        require "../app/controllers/" . $this->controller . ".php";
+        $this->controller = new $this->controller;
+
+        if (isset($url[1]))
+        {
+            if (method_exists($this->controller, $url[1]))
+            {
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+        $this->params = array_values($url);
+        call_user_func_array([$this->controller,$this->method],$this->params);
 
     }
     private function splitURL()
--- app/init.php
+++ app/init.php
@@ -4,3 +4,4 @@ require "../app/core/config.php";
 require "../app/core/database.php";
 require "../app/core/controller.php";
 require "../app/core/app.php";
+require "../app/core/function.php";
