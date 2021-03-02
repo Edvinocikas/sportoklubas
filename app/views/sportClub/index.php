54d3977f54c4bb708c307301b492c51539d8c9c0
edvie <edvi.edvi@yahoo.com>
2021-03-02 19:19:32 +0200

    index page complete

--- app/views/sportClub/index.php
+++ app/views/sportClub/index.php
@@ -7,9 +7,8 @@
     <link rel="stylesheet" href="<?= ASSETS ?>sportClub/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?= ASSETS ?>sportClub/fontawesome/css/all.min.css">
     <link rel="stylesheet" href="<?= ASSETS ?>sportClub/css/templatemo-style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Kalam:wght@300&display=swap" rel="stylesheet">
 </head>
-<body>
+<body style="background-color: #373737">
 <!-- Page Loader -->
 
 <div id="loader-wrapper">
@@ -53,7 +52,7 @@
 
 </div>
 
-<div class="container tm-container-content" style="padding-top: 20px;padding-bottom: 20px">
+<div class="container tm-container-content" style="padding-top: 20px;padding-bottom: 20px;margin-right: auto">
     <div class="row tm-gallery">
 
     <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12 border">
@@ -61,8 +60,7 @@
         <img src="<?= ASSETS ?>sportClub/img/6.jpg" alt="Image" class="img-fluid" style="height: 40%" width="100%">
         <div class="d-flex align-items-center justify-content-center">
             <h2 class="font">Baseinai</h2>
-            <div class="font">Fitus sporto klubo pasididžiavimas – 3 takelių 25 metrų ilgio plaukimo baseinas, kuriame
-                kūno ir sielos atgaivą patirs plaukimo aistruoliai.
+            <div class="font">Reguliarus lankymasis baseine padės atsikratyti nereikalingų kilogramų, sustangrins odą, sustiprins stuburą, pagerins miego kokybę ir bendrą savijautą.
             </div>
         </div>
 
@@ -75,7 +73,7 @@
         <img src="<?= ASSETS ?>sportClub/img/3.jpg" alt="Image" class="img-fluids" style="height: 40%" width="100%">
         <div class="d-flex align-items-center justify-content-center">
             <h2 class="font">Soliariumai</h2>
-            <div class="font"></div>
+            <div class="font">Gražūs nori būti visi – nenuostabu, kad šaltuoju metų laiku, soliariumų duris varsto tiek daug žmonių.</div>
         </div>
         <div class="d-flex justify-content-between tm-text-gray">
             <span class="tm-text-gray-light"></span>
@@ -87,7 +85,7 @@
         <img src="<?= ASSETS ?>sportClub/img/4.jpg" alt="Image" class="img-fluid" style="height: 40%" width="100%">
         <div class="d-flex align-items-center justify-content-center">
             <h2 class="font">Masažai</h2>
-            <div class="font"></div>
+            <div class="font" style="text-overflow: ellipsis;">Pasimėgaukite atpalaiduojančiu nugaros masažu, kuris šiltais, maloniais judesiais išsklaidys susikaupusią įtampą, nerimą ir nuovargį.</div>
         </div>
 
         <div class="d-flex justify-content-between tm-text-gray">
@@ -100,7 +98,7 @@
 
 <div class="mapouter ">
     <div style="width: 100%">
-        <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
+        <iframe width="100%" height="300px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"
                 src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=%20Saul%C4%97tekio%20al.%2015+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
 
         </iframe>
@@ -111,7 +109,7 @@
 <footer class="tm-bg-gray pt-5 pb-3 tm-text-gray tm-footer" style="height: 70px">
     <div class="container-fluid tm-container-small">
         <div class="row">
-            <div class="col-lg-8 col-md-7 col-12 px-5 mb-3">
+            <div class="col-lg-8 col-md-7 col-12 px-5 mb-3" style="padding-top: 20px;color: black">
                 2021 Edvin Vasilionok. All rights reserved.
             </div>
         </div>
